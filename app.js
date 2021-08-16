2b36220cbfa11f11f1ac0bbe576ef5fb673b61bd
Karanjeet Ahluwalia <karanjeet.ahluwalia@plusgrade.com>
2021-08-15 21:26:11 -0400

    add: reset button

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const btnFive = document.querySelector(".five");
 let tipAmount = document.querySelector(".amount-money");
 
 btnFive.addEventListener("click", () => {
-  tipAmount.innerHTML = (billInput.value * 5) / 100;
+  let tipAmountResult = (billInput.value * 5) / 100;
+  tipAmount.innerHTML = `<h4 class="amount-money">$${tipAmountResult}</h4>`;
 });
