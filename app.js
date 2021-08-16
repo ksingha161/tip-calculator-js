0ebd6628bd914ee1080e29eeeddeb415acafdeba
Karanjeet Ahluwalia <karanjeet.ahluwalia@plusgrade.com>
2021-08-16 13:17:56 -0400

    Add: calculate tip functionality

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 const billInput = document.querySelector(".total");
 const btnFive = document.querySelector(".five");
+const resetBtn = document.querySelector(".reset");
 let tipAmount = document.querySelector(".amount-money");
+let totalMoney = document.querySelector(".total-money");
 
-btnFive.addEventListener("click", () => {
-  let tipAmountResult = (billInput.value * 5) / 100;
-  tipAmount.innerHTML = `<h4 class="amount-money">$${tipAmountResult}</h4>`;
+window.addEventListener("click", (e) => {
+  let tipAmountResult;
+  switch (e.target.className) {
+    case "percent five":
+      tipAmountResult = (billInput.value * 5) / 100;
+      tipAmount.innerHTML = `<h4 class="amount-money">$${tipAmountResult}</h4>`;
+      totalMoney.innerHTML = `<h4 class="total-money">$${billInput.value}</h4>`;
+      break;
+
+    case "percent ten":
+      tipAmountResult = (billInput.value * 10) / 100;
+      tipAmount.innerHTML = `<h4 class="amount-money">$${tipAmountResult}</h4>`;
+      totalMoney.innerHTML = `<h4 class="total-money">$${billInput.value}</h4>`;
+      break;
+
+    case "percent fifteen":
+      tipAmountResult = (billInput.value * 15) / 100;
+      tipAmount.innerHTML = `<h4 class="amount-money">$${tipAmountResult}</h4>`;
+      totalMoney.innerHTML = `<h4 class="total-money">$${billInput.value}</h4>`;
+      break;
+
+    case "percent twent-five":
+      tipAmountResult = (billInput.value * 25) / 100;
+      tipAmount.innerHTML = `<h4 class="amount-money">$${tipAmountResult}</h4>`;
+      totalMoney.innerHTML = `<h4 class="total-money">$${billInput.value}</h4>`;
+      break;
+
+    case "percent fifty":
+      tipAmountResult = (billInput.value * 50) / 100;
+      tipAmount.innerHTML = `<h4 class="amount-money">$${tipAmountResult}</h4>`;
+      totalMoney.innerHTML = `<h4 class="total-money">$${billInput.value}</h4>`;
+      break;
+
+    default:
+      break;
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  tipAmount.innerHTML = `<h4 class="amount-money">$0</h4>`;
+  totalMoney.innerHTML = `<h4 class="amount-money">$0</h4>`;
 });
